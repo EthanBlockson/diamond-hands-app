02ed8fac19c80618b93973728e3d1eb8e549b142
sergeypokh <sergeypokh@gmail.com>
2024-06-15 15:10:51 +0100

    add eth address

--- src/data/USDT.js
+++ src/data/USDT.js
@@ -1,5 +1,5 @@
 export const USDT = {
-  1: '0x', // ethereum
+  1: '0x123', // ethereum
   42161: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', // arbitrum
   10: '0x', // optimism
   137: '0x', // polygon
