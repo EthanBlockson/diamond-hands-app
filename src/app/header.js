48f1831fedee1546c29777f6435127a591d5fc54
starcaps <capsdev@proton.me>
2024-06-24 12:13:49 +0300

    add beta alert

--- src/app/header.js
+++ src/app/header.js
@@ -30,75 +30,85 @@ export default function Header() {
   }, [address, chainId]);
 
   return (
-    <div className="header flex space-between center-baseline">
-      <div className="left flex row center-baseline gapped">
-        <Link className="logo" href="/">
-          <Image
-            src={`/img/brand/logo-long.png`}
-            width={135}
-            height={36}
-            alt=""
-          />
-        </Link>
-        <div className="menu flex row">
-          <Link
-            href="/hold"
-            className={pathname === '/hold' ? 'active' : undefined}
-          >
-            Hold
+    <>
+      <div className="alert flex center">
+        Currently in beta testing. Careful. All progress might be lost!
+      </div>
+      <div className="header flex space-between center-baseline">
+        <div className="left flex row center-baseline gapped">
+          <Link className="logo" href="/">
+            <Image
+              src={`/img/brand/logo-long.png`}
+              width={135}
+              height={36}
+              alt=""
+            />
           </Link>
-          {isConnected ? (
+          <div className="menu flex row">
             <Link
-              href={`/holdings/${chainIdToNameLowerCase[chainId]}/address/${address}`}
-              className={pathname.includes('/holdings') ? 'active' : undefined}
+              href="/hold"
+              className={pathname === '/hold' ? 'active' : undefined}
             >
-              My holdings
+              Hold
             </Link>
-          ) : null}
-          <Link
-            href="/earn"
-            className={pathname === '/earn' ? 'active' : undefined}
-          >
-            Earn
-          </Link>
+            {isConnected ? (
+              <Link
+                href={`/holdings/${chainIdToNameLowerCase[chainId]}/address/${address}`}
+                className={
+                  pathname.includes('/holdings') ? 'active' : undefined
+                }
+              >
+                My holdings
+              </Link>
+            ) : null}
+            <Link
+              href="/earn"
+              className={pathname === '/earn' ? 'active' : undefined}
+            >
+              Earn
+            </Link>
+          </div>
+        </div>
+        <div className="right flex row gapped">
+          {isConnected ? (
+            <>
+              <button
+                className="chain-selector flex row center-baseline"
+                onClick={() => open({ view: 'Networks' })}
+              >
+                <Image
+                  src={`/img/chains/${
+                    isSupportedChainId(chainId) ? chainId : 0
+                  }.svg`}
+                  width={25}
+                  height={25}
+                  alt=""
+                />
+                <Image
+                  src={`/img/icons/arrow-down.svg`}
+                  width={13}
+                  height={13}
+                  alt=""
+                />
+              </button>
+              <button
+                className="wallet flex row gapped center-baseline"
+                onClick={() => open()}
+              >
+                <div className="avatar"></div>
+                <div>{address && shortenAddress(address)}</div>
+              </button>
+            </>
+          ) : (
+            <button
+              className="connect"
+              onClick={() => open({ view: 'Connect' })}
+            >
+              Connect
+            </button>
+          )}
         </div>
       </div>
-      <div className="right flex row gapped">
-        {isConnected ? (
-          <>
-            <button
-              className="chain-selector flex row center-baseline"
-              onClick={() => open({ view: 'Networks' })}
-            >
-              <Image
-                src={`/img/chains/${
-                  isSupportedChainId(chainId) ? chainId : 0
-                }.svg`}
-                width={25}
-                height={25}
-                alt=""
-              />
-              <Image
-                src={`/img/icons/arrow-down.svg`}
-                width={13}
-                height={13}
-                alt=""
-              />
-            </button>
-            <button
-              className="wallet flex row gapped center-baseline"
-              onClick={() => open()}
-            >
-              <div className="avatar"></div>
-              <div>{address && shortenAddress(address)}</div>
-            </button>
-          </>
-        ) : (
-          <button className="connect" onClick={() => open({ view: 'Connect' })}>
-            Connect
-          </button>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
