a01405a81c7d4e66fd401305fbda7d6ab424d7f6
starcaps <capsdev@proton.me>
2024-06-24 12:24:05 +0300

    add dev mode switch, mute analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import '../app/scss/styles.scss';
+import { isDevMode } from '../../isDevMode';
 import { Suspense } from 'react';
 import Script from 'next/script';
 import Header from './header';
@@ -43,25 +44,29 @@ export default function RootLayout({ children }) {
             }}
             containerClassName="toasts"
           />
-          <Script id="metrika-counter" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-              ym(97648185, "init", {
-                    defer: true,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-              });`}
-          </Script>
-          <Suspense fallback={<></>}>
-            <YandexMetrika />
-          </Suspense>
-          <GoogleAnalytics gaId="G-HH1Z9FFRQF" />
+          {!isDevMode && (
+            <>
+              <Script id="metrika-counter" strategy="afterInteractive">
+                {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                      m[i].l=1*new Date();
+                      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+                      ym(97648185, "init", {
+                            defer: true,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                      });`}
+              </Script>
+              <Suspense fallback={<></>}>
+                <YandexMetrika />
+              </Suspense>
+              <GoogleAnalytics gaId="G-HH1Z9FFRQF" />
+            </>
+          )}
           <Header />
           {children}
           <Footer />
